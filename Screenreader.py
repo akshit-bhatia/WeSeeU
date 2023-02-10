b22c5dbaf1b3ab703749db9b5d106536c8afc512
mnx02 <67163094+mnx02@users.noreply.github.com>
2023-02-10 15:38:16 -0500

    Added code for POC
    Added TTS, speech recognization and object identification modules for POC

---
+++ Screenreader.py
@@ -0,0 +1,74 @@
+import pyautogui
+import time
+import speech_recognition as sr
+import subprocess
+import pyttsx3
+
+
+# Initializing TTS engine
+engine = pyttsx3.init()
+engine.setProperty("rate",130)
+
+# Building an object for Speech recognization
+r = sr.Recognizer()  
+
+# Setting up the game path and calling it
+Game_path = "D:\\Games\\Flash Games\\BallGame.exe"
+
+process = subprocess.Popen(Game_path, stderr = subprocess.PIPE)
+
+time.sleep(2)
+
+# Giving a subtitle file for TTS
+text_val = "C:\\Users\\Manav\\Desktop\\TestfileTTS.txt"
+file1 = open(text_val, 'r')
+Lines = file1.readlines()
+errormessage = "Sorry, I did not get that"
+language = 'en'  
+
+# Creating an object for playing the sound
+for line in Lines:
+    engine.say(line)
+    engine.runAndWait()
+
+# Asking user to choose an option and saying it in their microphone
+with sr.Microphone() as source:
+    print("Talk")
+    audio_text = r.listen(source)
+    print("Time over, thanks")
+# recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+    
+    try:
+        # using google speech recognition
+        print("Text: "+r.recognize_google(audio_text))
+    except:
+         engine.say(errormessage)
+         engine.runAndWait()
+
+option = r.recognize_google(audio_text)
+
+# building up the cases for selected option of a user
+if option == "play":
+    x, y = pyautogui.locateCenterOnScreen("play1.png", confidence = 0.8)
+    pyautogui.moveTo(x, y, 1)
+    pyautogui.click()
+
+elif option == "Instruction":
+    x, y = pyautogui.locateCenterOnScreen("info1.png", confidence = 0.8)
+    pyautogui.moveTo(x, y, 1)
+    pyautogui.click()
+
+elif option == "High Score":
+    x, y = pyautogui.locateCenterOnScreen("highscore1.png", confidence = 0.8)
+    pyautogui.moveTo(x, y, 1)
+    pyautogui.click()
+
+elif option == "Add Score":
+    x, y = pyautogui.locateCenterOnScreen("addscore.png", confidence = 0.8)
+    pyautogui.moveTo(x, y, 1)
+    pyautogui.click()
+
+elif option == "exit":
+    x, y = pyautogui.locateCenterOnScreen("quit1.png", confidence = 0.8)
+    pyautogui.moveTo(x, y, 1)
+    pyautogui.click()
